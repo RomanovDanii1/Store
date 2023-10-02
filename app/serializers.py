b38e341aa4ac7ac3b51215c515e71cea81d4b183
Daniil <romanovvv90@gmail.com>
2023-10-02 18:58:46 +0200

    Finish 1

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,4 @@
 from rest_framework import serializers
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
-from rest_framework import serializers
 from .models import Product
 
 
--- store/urls.py
+++ store/urls.py
@@ -3,13 +3,13 @@ from rest_framework.routers import DefaultRouter
 from app.views import ProductViewSet
 
 
-# Создайте объект DefaultRouter
+
 router = DefaultRouter()
 
-# Зарегистрируйте ваш ProductViewSet под именем 'product'
+
 router.register(r'product', ProductViewSet)
 
-# Определите URL-маршруты
+
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
